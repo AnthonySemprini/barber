4a8c161d3e0f3bab1ab25f1cc843984f6d7cb678
Anthony <semprini.anthony@gmail.com>
2023-09-14 17:46:05 +0200

    Redirection des routes invalide grace a RouteNotFoundSubscriber.php

---
+++ src/EventSubscriber/RouteNotFoundSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class RouteNotFoundSubscriber implements EventSubscriberInterface
+{
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        if($exception instanceof NotFoundHttpException) {
+            //generate the url for the desired redirect destination
+            $redirectUrl = $this->urlGenerator->generate('app_home');
+        
+            // Create a redirectResponce to the desired url
+            $response = new RedirectResponse($redirectUrl);
+
+            // Set the response to the event
+            $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+} 
