dc9e4740d5fd362b9bb3b68c1aec172207a2595c
Anthony <semprini.anthony@gmail.com>
2023-10-24 01:11:03 +0200

    css de la landing page + ajout couleur

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -47,8 +47,17 @@ class PanierController extends AbstractController
     #[Route('/panier/add/{id}', name:'app_panier_add')]
     public function add(int $id, SessionInterface $session){
         
-        $this->add($id);
-        
+       
+      $panier = $session->get('panier', []);
+
+        //verifie si le produit et deja en panier si oui 1 sinon ajoute le produit au panier
+        if(!empty($panier[$id])) {
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+        }
+
+        $session->set('panier', $panier);
         //redirige vers le panier
         return $this->redirectToRoute('app_panier');
     }
