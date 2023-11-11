3746a554675cf732c8de77bebe0c808677b1b7cb
Anthony <semprini.anthony@gmail.com>
2023-11-11 19:43:16 +0100

    modif name fonction createformcommande

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -24,58 +24,63 @@ class CommandeController extends AbstractController
         $this->managerRegistry = $managerRegistry;
     }
 
-    #[Route('/commande/infoClient', name: 'app_commande_infoClient')]
+    #[Route('/commande/confirmation', name: 'app_commande_confirmation')]
     public function createFormCommande(EntityManagerInterface $entityManager, Request $request,SessionInterface $session, ProduitRepository $produitRepository): Response
     {
+        // Vérifie si l'utilisateur a le rôle 'USER'. Si ce n'est pas le cas, l'accès à cette fonction est refusé.
         $this->denyAccessUnlessGranted('ROLE_USER');
-
+        // Récupère le panier de la session
         $panier = $session->get('panier', []);
-        // dd($panier);
-        $user = $this->getUser();
-        $now = new \DateTime();
-        $commande = new Commande();
-        $commande->setUser($user);
-        $commande->setDateCommande($now);
         
+        // Récupère l'utilisateur actuellement connecté.
+        $user = $this->getUser();
+
+        // Crée un nouvel objet DateTime
+        $now = new \DateTime();
+
+        // Crée une nouvelle instance de la classe Commande.
+        $commande = new Commande();
+        // Attribue l'utilisateur connecté à la commande.
+        $commande->setUser($user);
+        // Définit la date de la commande 
+        $commande->setDateCommande($now);
+         // Crée un formulaire pour l'objet Commande.
         $form = $this->createForm(CommandeFormType::class, $commande);
+        // Gère la requête HTTP actuelle et initialise le formulaire.
         $form->handleRequest($request);
 
+        // Vérifie si le formulaire a été soumis et est valide.
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->managerRegistry->getManager();
+            // Persiste et enregistre l'objet Commande dans la base de données.
             $entityManager->persist($commande);
             $entityManager->flush();
-            //dd($commande->getId());
+            
         
-        
+            // Parcourt chaque élément du panier.
             foreach($panier as $prod => $qtt){
             $produit = $produitRepository->find($prod);
+            // Crée une nouvelle instance de ProduitCommande.
             $produitCommande = new ProduitCommande();
+            // Attribue le produit et la quantité à ProduitCommande.
             $produitCommande->setProduit($produit);
-            //dd($produitCommande);
             $produitCommande->setQuantite($qtt);
             $produitCommande->setCommande($commande);
-            //dd($produitCommande);
             
+            // Persiste et enregistre l'objet ProduitCommande dans la base de données.
             $entityManager->persist($produitCommande);
-            //dd($produitCommande);
             $entityManager->flush();
         }
-        // $array = [];
-        // foreach($panier as $prod => $qtt)
-        // {
-        //     $array[] = $produitRepository->find($prod);
-        // }
-        // dd($array);
-        
+        // Redirige l'utilisateur vers la page de paiement de la commande.
         return $this->redirectToRoute('app_commande_paiement');
     }
        
-
+        // Si le panier est vide, redirige l'utilisateur vers la page d'accueil.
         if($panier === []){
-            //Le panier est vide, on retourne sur la homepage
             return $this->redirectToRoute('app_home');
         }
      
+        // Rendu de la vue avec le formulaire de commande.
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
             'commandeForm' => $form->createView()
