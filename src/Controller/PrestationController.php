b6544fe03b07db7aaabba651f5534834c91a2de0
Anthony <semprini.anthony@gmail.com>
2023-11-13 13:44:21 +0100

    ajout d'un script js pour régle la navbar

--- src/Controller/PrestationController.php
+++ src/Controller/PrestationController.php
@@ -18,7 +18,7 @@ class PrestationController extends AbstractController
         $pagination = $paginator->paginate(
             $prestationRepository->paginationQuery(),
             $request->query->get('page', 1),
-            6 //Nombre d'elements par page
+            8 //Nombre d'elements par page
         );
 
             return $this->render('prestation/index.html.twig', [
