cb4e4a5ba21184be29976f6aaa39c000acbbb3e3
Anthony <semprini.anthony@gmail.com>
2023-12-08 15:54:57 +0100

    empeche les users non verifier de se connecte

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -2,12 +2,14 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -22,16 +24,24 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, UserRepository $userRepository) 
     {
+        $this->userRepository = $userRepository;
     }
 
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
-
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
+        $user = $this->userRepository->findOneBy(['email'=> $email]);
+
+        if (!$user || !$user->isVerified()) {
+            $request->getSession()->getFlashBag()->add('error','Veuillez vérifier votre adresse email. Un lien de vérification a été envoyé à votre adresse mail. Vous devez cliquer sur ce lien pour activer votre compte et pouvoir vous connecter.
+            ');
+
+            throw new AuthenticationException('Votre compte pas verfie');
+        }
         return new Passport(
             new UserBadge($email),
             new PasswordCredentials($request->request->get('password', '')),
